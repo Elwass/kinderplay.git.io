6585da92c005905710b8473493ee4c5a19c8755e
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-01-14 05:08:16 +0700

    Center Our Programs header in gradient

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,8 @@
 import FeatureStrip from "../components/sections/FeatureStrip";
 import Hero from "../components/sections/Hero";
+import DaycareProgram from "../components/sections/DaycareProgram";
 import LearningFramework from "../components/sections/LearningFramework";
+import Readiness from "../components/sections/Readiness";
 
 const LandingPage = () => {
   return (
@@ -8,6 +10,8 @@ const LandingPage = () => {
       <Hero />
       <FeatureStrip />
       <LearningFramework />
+      <Readiness />
+      <DaycareProgram />
     </div>
   );
 };
