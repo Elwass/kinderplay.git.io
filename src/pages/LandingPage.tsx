f8bb30a2a1f1cfc0426ef91e92d54740fe086a78
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-01-14 07:21:05 +0700

    Fix program gradient seam

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,9 @@
 import FeatureStrip from "../components/sections/FeatureStrip";
 import Hero from "../components/sections/Hero";
+import DaycareProgram from "../components/sections/DaycareProgram";
 import LearningFramework from "../components/sections/LearningFramework";
+import OSHC from "../components/sections/OSHC";
+import Readiness from "../components/sections/Readiness";
 
 const LandingPage = () => {
   return (
@@ -8,6 +11,9 @@ const LandingPage = () => {
       <Hero />
       <FeatureStrip />
       <LearningFramework />
+      <Readiness />
+      <DaycareProgram />
+      <OSHC />
     </div>
   );
 };
